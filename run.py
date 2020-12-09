2c3204cf7bf930ee322d037074790d442b3d3f6d
Michael Patel <michael.patel94@gmail.com>
2020-12-09 13:07:45 -0500

    Reading csv

---
+++ run.py
@@ -0,0 +1,51 @@
+"""
+Michael Patel
+December 2020
+
+Project description:
+    Perform data analysis and visualization of viewing habits
+
+"""
+################################################################################
+# Imports
+import os
+import pandas as pd
+
+
+################################################################################
+# constants
+DATA_CSV_FILEPATH = os.path.join(os.getcwd(), "data.csv")
+
+
+################################################################################
+# Main
+if __name__ == "__main__":
+    df = pd.read_csv(DATA_CSV_FILEPATH)
+
+    # ----- NUMBER OF MOVIES ----- #
+    num_movies = len(df)
+    #print(f'Number of movies: {num_movies}')
+
+    # ----- GENRES ----- #
+    genres = df["Genre"].unique()
+    genre_counts = df["Genre"].value_counts()
+
+    #print(genre_counts)
+    # plot number per genre
+
+    # ----- COMPLETION PERCENTAGE ----- #
+    num_finished_yes = len(df.loc[df["Finished"] == "Yes"])
+    completion_percentage = num_finished_yes / num_movies
+    #print(completion_percentage)
+
+    # ----- SCORE ----- #
+    # average score per genre
+
+    # ----- STREAMING SERVICE ----- #
+    # number per streaming service
+
+    # pie chart streaming service
+
+    # ----- FIRST LETTER ----- #
+    # number per first letter in title
+    quit()
