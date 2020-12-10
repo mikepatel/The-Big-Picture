a22bbe5a02d50f04a5a6d552c9d936486839f06f
Michael Patel <michael.patel94@gmail.com>
2020-12-10 14:36:04 -0500

    Adding counts dataframe

--- run.py
+++ run.py
@@ -9,8 +9,13 @@ Project description:
 ################################################################################
 # Imports
 import os
+import numpy as np
 import pandas as pd
 import string
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.ticker as ticker
+import matplotlib.animation as animation
 
 
 ################################################################################
@@ -23,16 +28,68 @@ DATA_CSV_FILEPATH = os.path.join(os.getcwd(), "data.csv")
 if __name__ == "__main__":
     df = pd.read_csv(DATA_CSV_FILEPATH)
 
+    data = {}
+
     # ----- NUMBER OF MOVIES ----- #
     num_movies = len(df)
     #print(f'Number of movies: {num_movies}')
 
     # ----- GENRES ----- #
     genres = df["Genre"].unique()
-    genre_counts = df["Genre"].value_counts()
+    genres = sorted(genres)  # alphabetize
+    #print(genres)
+    genre_counts = {}
+
+    # initialize
+    for g in genres:
+        genre_counts[g] = 0
+
+    for index, row in df.iterrows():
+        genre_counts[row["Genre"]] += 1
+
+    #genre_counts = df["Genre"].value_counts()
+    genre_counts = pd.Series(genre_counts)
     #print(genre_counts)
 
     # plot number per genre
+    """
+    plt.style.use("dark_background")
+    fig, ax = plt.subplots(figsize=(15, 10))
+    colours = cm.rainbow(np.linspace(0, 1, len(genres)))
+    plt.bar(genres, genre_counts[genres], color=colours)
+    plt.show()
+    """
+
+    # racing bar
+    plt.style.use("dark_background")
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    #print(counts)
+
+    def draw_chart(frame):
+        df = pd.read_csv(DATA_CSV_FILEPATH)
+        df = df.head(frame)
+
+        counts = {}
+        for g in genres:
+            counts[g] = 0
+
+        counts_df = pd.DataFrame(counts.items(), columns=["Genre", "Count"])
+
+        for index, row in df.iterrows():
+            g = row["Genre"]
+            #print(g)
+            counts_df.loc[counts_df["Genre"] == g, "Count"] += 1
+
+        counts_df["Rank"] = counts_df["Count"].rank(method="first")
+        ax.clear()
+        ax.bar(counts_df["Rank"], counts_df["Count"])
+
+    gif_filepath = os.path.join(os.getcwd(), "genre_race.gif")
+    animator = animation.FuncAnimation(fig, draw_chart, frames=range(len(df)), interval=800)
+    animator.save(gif_filepath, writer="imagemagick")
+
+    quit()
 
     # ----- COMPLETION PERCENTAGE ----- #
     num_finished_yes = len(df.loc[df["Finished"] == "Yes"])
@@ -95,5 +152,9 @@ if __name__ == "__main__":
         letter_counts[letter] += 1
 
     print(letter_counts)
+
+    # plot
+
+    # racing bar
 
     quit()
